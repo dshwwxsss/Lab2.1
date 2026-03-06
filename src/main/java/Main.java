84bdcc2909c814163ad81fdae667eeb38af77406
dshwwxsss <dashka.zdo@mail.ru>
2026-03-06 21:49:57 +0300

    add ReportLine in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,12 @@
 import domain.Report;
+import domain.ReportLine;
 import domain.ReportStatus;
 
 public class Main {
     public static void main(String[] args) {
-        Report myReport = new Report(1, "Лабораторная работа №1", ReportStatus.NEW);
-        System.out.println("Отчёт успешно создан");
-        System.out.println("ID отчёта: " + myReport.getId());
-        System.out.println("Название: " + myReport.getName());
-        System.out.println("Статус: " + myReport.getStatus());
-        System.out.println("Проверка пройдена");
+        Report report = new Report(1, "Лабораторная работа №1", ReportStatus.IN_PROGRESS);
+        ReportLine line = new ReportLine(101, "Кислотность", 7.4, "pH");
+        System.out.println("Отчёт: " + report.getName() + "[" + report.getStatus() + "]");
+        System.out.println("Данные: " + line.getParam() + " = " + line.getValue() + " " + line.getUnit());
     }
 }
