f06ac0d6f2e0bfad575deae5695f86b40dd8f8a1
dshwwxsss <dashka.zdo@mail.ru>
2026-03-06 21:20:01 +0300

    test Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import domain.Report;
+import domain.ReportStatus;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello?");
-//ты крутая
-        // все будет круто сдадим прогу на 100
-
+        Report myReport = new Report(1, "Лабораторная работа №1", ReportStatus.NEW);
+        System.out.println("Отчёт успешно создан");
+        System.out.println("ID отчёта: " + myReport.getId());
+        System.out.println("Название: " + myReport.getName());
+        System.out.println("Статус: " + myReport.getStatus());
+        System.out.println("Проверка пройдена");
     }
 }
