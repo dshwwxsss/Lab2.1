e1b2f78bbd998e6e6318c6e6e876d44e368c2b5e
dshwwxsss <dashka.zdo@mail.ru>
2026-03-06 22:20:22 +0300

    add ArrayList

--- src/main/java/domain/Report.java
+++ src/main/java/domain/Report.java
@@ -8,12 +8,23 @@ public class Report {
     private ReportStatus status;
     private Instant createdAt;
 
+    private List<ReportLine> lines = new ArrayList<>();
+
     public Report(long id, String name, ReportStatus status) {
         this.id = id;
         this.name = name;
         this.status = status;
         this.createdAt = Instant.now();
     }
+
+    public void addLine(ReportLine line) {
+        return lines;
+    }
+
+    public List<ReportLine> getLines() {
+        return lines;
+    }
+
     public long getId() { return id; }
     public String getName() { return name; }
     public ReportStatus getStatus() { return status; }
