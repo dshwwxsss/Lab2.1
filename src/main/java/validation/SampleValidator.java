95ff92ce73698dc1b345694abda6de22a02138c4
dshwwxsss <dashka.zdo@mail.ru>
2026-03-24 19:15:19 +0300

    add validators for domain Sample

---
+++ src/main/java/validation/SampleValidator.java
@@ -0,0 +1,11 @@
+package validation;
+
+import domain.Sample;
+
+public class SampleValidator {
+    public static void validate(Sample sample) throws ValidationException {
+        if (sample.getName() == null || sample.getName().trim().isEmpty()) {
+            throw new ValidationException("Название образца не может быть пустым");
+        }
+    }
+}
