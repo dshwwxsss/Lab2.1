2c7284fe5cd0976588b3e05f8265177859aa3044
dshwwxsss <dashka.zdo@mail.ru>
2026-03-24 19:22:55 +0300

    add RepUpdatelineCommand

---
+++ src/main/java/cli/commands/RepUpdatelineCommand.java
@@ -0,0 +1,44 @@
+package cli.commands;
+
+import cli.Command;
+import cli.Environment;
+import validation.ValidationException;
+import java.util.List;
+
+public class RepUpdatelineCommand extends Command {
+    public RepUpdatelineCommand(Environment env) {
+        super(env);
+    }
+
+    @Override
+    public void checkArgs(List<String> args) throws ValidationException {
+        if (args.size() < 2) {
+            throw new ValidationException("Использование: rep_updateline <line_id> field=value ...");
+        }
+        try {
+            Long.parseLong(args.get(0));
+        } catch (NumberFormatException e) {
+            throw new ValidationException("ID строки должен быть целым числом");
+        }
+        for (int i = 1; i < args.size(); i++) {
+            if (!args.get(i).contains("=")) {
+                throw new ValidationException("Аргумент должен быть вида field=value");
+            }
+        }
+    }
+
+    @Override
+    public void execute(List<String> args) throws ValidationException {
+        long lineId = Long.parseLong(args.get(0));
+        for (int i = 1; i < args.size(); i++) {
+            String[] kv = args.get(i).split("=", 2);
+            env.getReportLineService().updateLine(lineId, kv[0], kv[1]);
+        }
+        System.out.println("OK");
+    }
+
+    @Override
+    public String getHelp() {
+        return "rep_updateline <line_id> field=value ... – изменить строку";
+    }
+}
